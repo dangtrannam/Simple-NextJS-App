4cb24760c501df7437ddb8249e98fc32f349b4e3
dangtrannam <dangtrannam2001@gmail.com>
2021-08-15 18:03:26 +0700

    Render list of meetups

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUP = [
+  {
+    id: "1",
+    name: "Dummy meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/PonNagarChamTowers.jpg/1280px-PonNagarChamTowers.jpg",
+    address: "123 Fake Street, Fake City,Fake State,Fake Country",
+  },
+  {
+    id: "2",
+    name: "Second Dummy meetup",
+    image:
+      "https://cdn.vntrip.vn/cam-nang/wp-content/uploads/2017/08/bo-bien-vinpearl-bai-dai-nha-trang2-e1504670930803.jpg",
+    address: "123 Fake Street, Fake City,Fake State,Fake Country",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUP} />;
+}
+
+export default HomePage;
