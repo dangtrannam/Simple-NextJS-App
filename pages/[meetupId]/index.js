d66164c74bc92e0601d1592d4b77d4132a3bec97
dangtrannam <dangtrannam2001@gmail.com>
2021-08-16 10:44:56 +0700

    getStaticPath

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,8 +1,42 @@
 import { Fragment } from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
-  return <MeetupDetail  /> 
+function MeetupDetails(props) {
+  return <MeetupDetail  />;
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "1",
+        },
+      },
+      {
+        params: {
+          meetupId: "2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/PonNagarChamTowers.jpg/1280px-PonNagarChamTowers.jpg",
+        id: meetupId,
+        title: "Pon Nagar Cham Towers",
+        address: "Pon Nagar, New Delhi",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
