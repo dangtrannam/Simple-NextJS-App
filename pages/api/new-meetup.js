238e0dbaed2abc1d1b1d3f5372240f588bd01130
dangtrannam <dangtrannam2001@gmail.com>
2021-08-16 11:30:15 +0700

    connect mongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,9 +1,21 @@
+import {MongoClient} from 'mongodb'
+import { async } from 'regenerator-runtime';
 //api/new-meetup
 
-function handler(req,res){
+async function handler(req,res){
     if(req.method == "POST"){
         const data = req.body;
-        const {title, image, address, description } = data;
+
+        const client = await MongoClient.connect("mongodb+srv://nam:1qaz@cluster0.nwfyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", (err, db) => {})
+        const db = client.db()
+        const meetupsCollection = db.collection('meetups')
+
+        const result = await meetupsCollection.insertOne(data)
+
+        console.log(result)
+        client.close()
+
+        res.status(201).json({message: 'Meetup inserted!'})
     }
 }
 
