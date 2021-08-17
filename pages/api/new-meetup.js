3a251dcf923f223b3848ebdd21f1b9a3c9e26b10
dangtrannam <dangtrannam2001@gmail.com>
2021-08-17 16:22:14 +0700

    Sending HTTP Requests to API Routes

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -6,7 +6,7 @@ async function handler(req,res){
     if(req.method == "POST"){
         const data = req.body;
 
-        const client = await MongoClient.connect("mongodb+srv://nam:1qaz@cluster0.nwfyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", (err, db) => {})
+        const client = await MongoClient.connect("mongodb+srv://nam:1qaz@cluster0.nwfyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
         const db = client.db()
         const meetupsCollection = db.collection('meetups')
 
